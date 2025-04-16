8a2110fe905cc0d9c9a32c5a72d8b22d9776e5ef
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-04-16 18:43:51 +0100

    First commit

---
+++ wolds-hr-api/Helper/DepartmentHelper.cs
@@ -0,0 +1,19 @@
+﻿using employee_test_api.Domain;
+
+namespace employee_test_api.Helpers;
+
+public static class DepartmentHelper
+{
+    public static List<Department> CreateDepartments(List<Department> departments)
+    {
+        departments.Add(new Department() { Id = 1, Name = "Human Resource" });
+        departments.Add(new Department() { Id = 2, Name = "IT" });
+        departments.Add(new Department() { Id = 3, Name = "Finance" });
+        departments.Add(new Department() { Id = 4, Name = "Marketing" });
+        departments.Add(new Department() { Id = 5, Name = "Operations" });
+        departments.Add(new Department() { Id = 6, Name = "QA" });
+        departments.Add(new Department() { Id = 7, Name = "Accounts" });
+
+        return departments;
+    }
+}
---
+++ wolds-hr-api/Helper/Dto/Responses/FailedValidationResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace employee_test_api.Helper.Dto.Responses;
+
+public class FailedValidationResponse
+{
+    public List<String> Errors { get; set; } = [];
+}
