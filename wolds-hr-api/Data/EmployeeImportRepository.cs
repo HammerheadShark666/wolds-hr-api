87837a01e6925ad8db05536723abaa6a751d994f
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-06-24 15:00:54 +0100

    Add employee import repository

---
+++ wolds-hr-api/Data/EmployeeImportRepository.cs
@@ -0,0 +1,28 @@
+﻿using wolds_hr_api.Data.Context;
+using wolds_hr_api.Data.Interfaces;
+using wolds_hr_api.Domain;
+
+namespace wolds_hr_api.Data;
+
+public class EmployeeImportRepository(AppDbContext context) : IEmployeeImportRepository
+{
+    private readonly AppDbContext _context = context;
+
+    public List<EmployeeImport> Get()
+    {
+        return [.. _context.EmployeeImports.OrderByDescending(a => a.Date)];
+    }
+
+    public EmployeeImport Add()
+    {
+        EmployeeImport employeeImport = new()
+        {
+            Date = DateTime.Now
+        };
+
+        _context.EmployeeImports.Add(employeeImport);
+        _context.SaveChangesAsync();
+
+        return employeeImport;
+    }
+}
---
+++ wolds-hr-api/Data/Interfaces/IEmployeeImportRepository.cs
@@ -0,0 +1,9 @@
+﻿using wolds_hr_api.Domain;
+
+namespace wolds_hr_api.Data.Interfaces;
+
+public interface IEmployeeImportRepository
+{
+    List<EmployeeImport> Get();
+    EmployeeImport Add();
+}
