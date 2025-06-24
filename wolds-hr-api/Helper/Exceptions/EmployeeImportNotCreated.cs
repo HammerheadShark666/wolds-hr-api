6cfe7c666a9903f1cb5009f92c6c68e19c357003
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-06-24 16:01:26 +0100

    Check employee import record created

---
+++ wolds-hr-api/Helper/Exceptions/EmployeeImportNotCreated.cs
@@ -0,0 +1,18 @@
+﻿namespace wolds_hr_api.Helper.Exceptions;
+
+public class EmployeeImportNotCreated : Exception
+{
+    public EmployeeImportNotCreated()
+    {
+    }
+
+    public EmployeeImportNotCreated(string message)
+        : base(message)
+    {
+    }
+
+    public EmployeeImportNotCreated(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
